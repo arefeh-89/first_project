c06addac5f5a5a08578597d5b336f924de7e96e3
fatemeh  rastegarnejad <f.rastegarnejad@internal.tosantechno.com>
2021-07-04 16:27:20 +0430

    changing variable name

--- password.py
+++ password.py
@@ -2,6 +2,6 @@ import re
 
 pattern = r"^[\w|\d|@|#|\$|%]{7,}\d$"
 password = input('please enter a password: ')
-psw = re.compile(password)
-check = psw.search(pattern)
+psw1 = re.compile(password)
+check = psw1.search(pattern)
 print(check)
